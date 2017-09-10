ade2ded74070bf52888f6cf9ac71d4ca52fb0dd6
lex alexander <l.alexander10@gmail.com>
2017-09-09 18:59:56 -0700

    Can now download both worldstar videos and youtube embedded videos.

--- app.js
+++ app.js
@@ -3,19 +3,57 @@ const fs = require('fs');
 const cheerio = require('cheerio')
 const $ = cheerio.load('<h2 class="title">Hello world</h2>')
 const request = require('request')
+const ytdl = require('ytdl-core');
+const YOUTUBE_URL = `https://www.youtube.com/watch?v=`
 
-request('http://www.worldstarhiphop.com/videos/video.php?v=wshhuEi723eo7GE9i204', (err, res) => {
-	const $ = cheerio.load(res.body)
-	let vid = $('video').children()
-	let foundVideoSrc; 
-	for(var key in vid) {
-		let keyIsInt = Number.isInteger(parseInt(key))
-		if(keyIsInt && vid[key].attribs['type'] === 'video/mp4') {
-			foundVideoSrc = vid[key].attribs['src']
+
+let arr = ['http://www.worldstarhiphop.com/videos/video.php?v=wshh3xxPMf0cS17Csd3s', 
+'http://www.worldstarhiphop.com/videos/video.php?v=wshhwrHyvi8uMFw3E576'].forEach((url) => {
+	request(url, (err, res) => {
+		const $ = cheerio.load(res.body)
+		let vidSrc = $('source')
+		let iframeEl = $('iframe')
+		if(vidSrc.length === 0) {
+			downloadYoutubeVideo(iframeEl, $('title').text() || $('h1').text())
 		}
+		if(vidSrc.length > 0) {
+			downloadWorldstarHipVideo(vidSrc, $('title').text() || $('h1').text())
+		}
+	})
+})
+
+
+function downloadWorldstarHipVideo(sourceEl, title) {
+	let sourceElLength = sourceEl.length
+	let worldStarUrl;
+	for(var i = 0; i < sourceElLength; i++) {
+		if(sourceEl[i].attribs['type'] === 'video/mp4') {
+			const {src} = sourceEl[i].attribs
+			worldStarUrl = src
+		} 
 	}
-	const file = fs.createWriteStream("another.mp4")
-	http.get(foundVideoSrc, function(response) {
+	const file = fs.createWriteStream(`${title}.mp4`)
+	http.get(worldStarUrl, function(response) {
   	response.pipe(file);
 	});
-})
+	file.on('close', (err) => {
+		console.log(`Done downloading ${title} video`)
+	})
+}
+
+function downloadYoutubeVideo(iFrameEl, title) {
+	let iFrameLength = iFrameEl.length
+	let youtubeId;
+	for(var i = 0; i < iFrameLength; i++) {
+		if(iFrameEl[i].attribs['src'].includes('youtube')) {
+			youtubeId = iFrameEl[0].attribs['src'].replace(/(?:http:\/\/www.youtube.com\/embed\/)/, "").split("?")[0]
+		}
+	}
+	const file = fs.createWriteStream(`${title}.mp4`)
+	ytdl(`${YOUTUBE_URL}${youtubeId}`, { 
+		filter: function(format) { return format.container === 'mp4'; } })
+		.pipe(file);
+		file.on('close', (err) => {
+			console.log(`Done downloading ${title} video`)
+		})
+}
